27caf51ec1ccb2c3f3cbc5dd14fae410079c5d51
dokhung <ehhiop123@naver.com>
2023-10-20 12:13:36 +0900

    feat/new

---
+++ MAUI_Project_CroseApp/MAUI_Project_CroseApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUI_Project_CroseApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MAUI_Project_CroseApp/MAUI_Project_CroseApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI_Project_CroseApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
